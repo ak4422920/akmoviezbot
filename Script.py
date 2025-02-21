2d6761b2f84dea530d9a7116075ea05ae268370e
ak4422920 <ak4422920@gmail.com>
2025-02-21 11:32:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/cineoriginals">𝘾𝙄𝙉𝙀𝙊𝙍𝙄𝙂𝙄𝙉𝘼𝙇𝙎</a>」</b>"""
+    FILE_CAPTION = """<b> @AKMOVIESHUBBACKUP ~{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/cineoriginals">𝘾𝙄𝙉𝙀𝙊𝙍𝙄𝙂𝙄𝙉𝘼𝙇𝙎</a>」</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
@@ -208,8 +208,7 @@ User - {}"""
 **🎃 Genres:** {genres}
 **📆 Year:** {year}
 **⭐ Rating:** {rating} / 10
-Bʏ @AKMOVIEBOTZ
-</b>"""
+Bʏ @AKMOVIEBOTZ </b>"""
 
     PREPLANS_TXT = """<b>👋 ʜᴇʏ {},
 
